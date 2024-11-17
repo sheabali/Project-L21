4cd2faec03b5188c04baf42e887b39a82c033d72
Sheab Ali <sheabali@Sheab-MacBook-Pro.local>
2024-11-18 00:19:35 +0600

    7-9 Routing in express.js

--- dist/app.js
+++ dist/app.js
@@ -9,11 +9,45 @@ const port = 3000;
 // parsers 
 app.use(express_1.default.json());
 app.use(express_1.default.text());
-app.get('/', (req, res) => {
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/courses', courseRouter);
+userRouter.get('/create-user', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User is Created sucessfully",
+        data: user
+    });
+});
+courseRouter.post('/create-course', (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "course created successfully",
+        data: course,
+    });
+});
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+app.get('/', logger, (req, res) => {
     res.send('Hello developer!!!');
 });
-app.post('/', (req, res) => {
-    console.log(req.body);
+// how to get params id 
+// app.post('/:userId', (req: Request, res: Response) => {
+//   console.log(req.params)
+//   res.send({
+//     message: "successfully received data"
+//   })
+// })
+// how to find query 
+app.post('/', logger, (req, res) => {
+    console.log(req.query);
     res.send({
         message: "successfully received data"
     });
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response, } from 'express'
 const app = express()
 const port = 3000
 
@@ -7,12 +7,57 @@ const port = 3000
 app.use(express.json())
 app.use(express.text())
 
-app.get('/', (req: Request, res: Response) => {
+
+const userRouter = express.Router()
+const courseRouter = express.Router()
+
+app.use('/api/v1/user', userRouter)
+app.use('/api/v1/courses', courseRouter)
+
+userRouter.get('/create-user', (req: Request, res: Response) => {
+const user = req.body
+console.log(user)
+
+res.json ({
+  success: true,
+  message: "User is Created sucessfully",
+  data: user
+})
+
+})
+
+
+courseRouter.post('/create-course', (req: Request, res: Response) => {
+  const course = req.body
+  console.log(course)
+
+  res.json({
+    success: true,
+    message: "course created successfully",
+    data: course,
+  })
+})
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname)
+  next()
+}
+
+app.get('/', logger, (req: Request, res: Response) => {
   res.send('Hello developer!!!')
 })
 
-app.post('/', (req: Request, res: Response) => {
-  console.log(req.body)
+// how to get params id 
+// app.post('/:userId', (req: Request, res: Response) => {
+//   console.log(req.params)
+//   res.send({
+//     message: "successfully received data"
+//   })
+// })
+
+// how to find query 
+app.post('/', logger, (req: Request, res: Response) => {
+  console.log(req.query)
   res.send({
     message: "successfully received data"
   })
